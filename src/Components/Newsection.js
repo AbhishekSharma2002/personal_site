a9a428fb7eaf30a4469034a6d16ac904d2bba3f5
Abhishek sharma <abhisheksharma73580@gmail.com>
2023-09-21 22:04:50 +0530

    password generator functionality added

--- src/Components/Newsection.js
+++ src/Components/Newsection.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 
 export default function Newsection() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     <>
       <div className='dark:text-white dark:bg-slate-800 h-screen'>
@@ -22,14 +57,14 @@ export default function Newsection() {
           <div className="flex shadow rounded-lg overflow-hidden mb-4">
             <input
               type="text"
-              // value={password}
+              value={password}
               className="outline-none w-full py-1 px-3"
               placeholder="Password"
               readOnly
-            // ref={passwordRef}
+            ref={passwordRef}
             />
             <button
-              // onClick={copyPasswordToClipboard}
+              onClick={copyPasswordToClipboard}
               className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
             >copy</button>
 
@@ -40,31 +75,31 @@ export default function Newsection() {
                 type="range"
                 min={6}
                 max={100}
-              // value={length}
-              // className='cursor-pointer'
-              // onChange={(e) => { setLength(e.target.value) }}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
               />
-              {/* <label>Length: {length}</label> */}
+              <label>Length: {length}</label>
             </div>
             <div className="flex items-center gap-x-1">
               <input
                 type="checkbox"
-                // defaultChecked={numberAllowed}
+                defaultChecked={numberAllowed}
                 id="numberInput"
-              // onChange={() => {
-              //   setNumberAllowed((prev) => !prev);
-              // }}
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
               />
               <label htmlFor="numberInput">Numbers</label>
             </div>
             <div className="flex items-center gap-x-1">
               <input
                 type="checkbox"
-                // defaultChecked={charAllowed}
+                defaultChecked={charAllowed}
                 id="characterInput"
-              // onChange={() => {
-              //   setCharAllowed((prev) => !prev)
-              // }}
+              onChange={() => {
+                setCharAllowed((prev) => !prev)
+              }}
               />
               <label htmlFor="characterInput">Characters</label>
             </div>
